33c6e6447c58e1867b93940561740b9a11018825
applevaldesco19@gmail.com <applevaldesco19@gmail.com>
2024-05-28 23:02:02 +0800

    better

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ diseases_list = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic c
                  19: 'Hepatitis B', 20: 'Hepatitis C', 21: 'Hepatitis D', 22: 'Hepatitis E', 3: 'Alcoholic hepatitis',
                  36: 'Tuberculosis', 10: 'Common Cold', 34: 'Pneumonia', 13: 'Dimorphic hemmorhoids(piles)',
                  18: 'Heart attack', 39: 'Varicose veins', 26: 'Hypothyroidism', 24: 'Hyperthyroidism',
-                 25: 'Hypoglycemia', 31: 'Osteoarthristis', 5: 'Arthritis', 0: '(vertigo) Paroymsal Positional Vertigo',
+                 25: 'Hypoglycemia', 31: 'Osteoarthristis', 5: 'Arthritis', 0: '(Vertigo) Paroxysmal Positional Vertigo',
                  2: 'Acne', 38: 'Urinary tract infection', 35: 'Psoriasis', 27: 'Impetigo'}
 
 
